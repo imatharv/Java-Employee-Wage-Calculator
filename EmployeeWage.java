c3db03549669ca348ac83920b258dc270fa72ed1
Atharva Joshi <iamatharvaj@google.com>
2021-05-05 14:25:06 +0530

    Added: Solution to check employee is present or abscent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,10 @@
 public class EmployeeWage {
 	public static void main(String[] args) {
-		System.out.println("Welcome to employee wage builder problem.");
+		int isFullTime = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isFullTime)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is abscent");
 	}
 }
