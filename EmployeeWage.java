b1afb63350a6053febc6eb71d03e8d56c1c035ba
Atharva Joshi <iamatharvaj@google.com>
2021-05-07 18:35:33 +0530

    Added: Solution for calculating employee wage for multiple companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,48 +1,54 @@
 public class EmployeeWage {
-	public static final int empWagePerHour = 20;
-	public static final int workingHours = 100;
+
 	public static final int isPartTime = 1;
 	public static final int isFullTime = 2;
-	public static final int workingDays = 30;
 
-	public static void calculateWage() 
+	public final String companyName;
+	public final int empWagePerHour;
+	public final int workingDays;
+	public final int workingHours;
+
+	public EmployeeWage(String companyName, int empWagePerHour, int workingHours, int workingDays)
+	{
+		this.companyName = companyName;
+		this.empWagePerHour = empWagePerHour;
+		this.workingHours = workingHours;
+		this.workingDays = workingDays;
+	}
+
+	public int calculateWage() 
 	{
 		int empHours = 0;
 		int empWage = 0;
 		int totalWorkingDays = 0;
 		int totalEmpWage = 0;
 		int totalWorkingHours = 0;
-		int day = 0;
+
 		while(totalWorkingHours<workingHours && totalWorkingDays<workingDays)
 		{	
-			day++;
 			totalWorkingDays++;
 			int empCheck=((int)Math.floor(Math.random()*10))%3;
 			switch(empCheck)
 			{
 				case isFullTime:
 					empHours = 8;
-					System.out.println("Employee is present full-time on day " + day + " for " + empHours + " hours");
 					break;
 				case isPartTime:
 					empHours = 4;
-					System.out.println("Employee is present part-time on day " + day + " for " + empHours + " hours");
 					break;
 				default:
 					empHours = 0;
-					System.out.println("Employee is abscent on day " + day);
 			}
 			totalWorkingHours += empHours;
-			empWage = empHours * empWagePerHour;
-			totalEmpWage += empWage;
 		}
-		System.out.println();
-		System.out.println("Maximum working hours: " + totalWorkingHours);
-		System.out.println("Maximum working days: " + totalWorkingDays);
-		System.out.println("Total employee's wage for maximum of " + workingHours + " hours or " + workingDays + " days is: " + totalEmpWage);
+		return totalWorkingHours * empWagePerHour;
 	}
+
 	public static void main(String[] args)
 	{
-		calculateWage(); // Directly calling function without creating obj 'cause its a static one.
+		EmployeeWage dmart = new EmployeeWage("Dmart", 20, 100, 30);
+		EmployeeWage walmart = new EmployeeWage("Walmart", 30, 100, 30);
+		System.out.println("Total employee's wage of " + dmart.companyName + " for maximum of " + dmart.workingHours + " hours or " + dmart.workingDays + " days is: " + dmart.calculateWage());
+		System.out.println("Total employee's wage of " + walmart.companyName + " for maximum of " + walmart.workingHours + " hours or " + walmart.workingDays + " days is: " + walmart.calculateWage());
 	}
 }
